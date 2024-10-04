b920d2c02f9ad373b5f3413498d2709d6a1619b4
Faiz <supremomelao@gmail.com>
2024-10-04 13:52:16 -0300

    pequena mudança

--- queue.py
+++ queue.py
@@ -1,6 +1,6 @@
 class No:
     def __init__(self,Name,age):
-        self.data = data
+        self.data = Name 
         self.next = None
         self.priori = None
 #classe queue teste
@@ -23,3 +23,18 @@ class Queue:
             
             aux.next = No(data,datage)
             self.val +=1
+
+    def displayList(self):
+        disaux = self.head
+        while disaux :
+            print(disaux.data, end=" -> " if disaux.next else "\n")
+            disaux = disaux.next
+
+
+
+
+
+
+
+
+ # type: ignore
