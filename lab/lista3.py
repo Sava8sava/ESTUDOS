b49bb034943d5fc30f384746e0f4480511228473
Faiz <supremomelao@gmail.com>
2024-10-08 18:28:13 -0300

    l3

--- lab/lista3.py
+++ lab/lista3.py
@@ -1,28 +1,33 @@
 pessoas ={}
 
 def add_pessoa(mapa,cpf,nome,ende,tele):
-    mapa[cpf] = {
-        "cpf" : cpf,
+    mapa[str(cpf)] = {
+        "cpf" : str(cpf),
         "nome": nome,
         "endereço" : ende,
         "telefone" : tele
     }
 
-def buscar(mapa,cara):
+def buscar_cpf(mapa,cara):
     if cara in mapa:
         return mapa[cara]
-    else:    
-         return 'Pessoa não encontrada'
+     
+    return 'Pessoa não encontrada'
+
+def buscar_tele(mapa,tele):
+     for pessoa in mapa.values():
+        if pessoa["telefone"] == tele:
+            return pessoa
+    
+     return 'Pessoa não encontrada'
+
        
      
 if __name__ == "__main__":
 
     while True:
 
-        op = int(input("1. Inserir pessoa:\n2. Listar pessoas cadastradas:\n3. Buscar pessoa por CPF:\n4. Buscar pessoa por Telefone:\n5. Remover pessoa por CPF:\n6.Sair:\n escolha a opção"))
-
-
-        
+        op = int(input("1. Inserir pessoa:\n2. Listar pessoas cadastradas:\n3. Buscar pessoa por CPF:\n4. Buscar pessoa por Telefone:\n5. Remover pessoa por CPF:\n6.Sair:\nescolha a opção"))
 
         if op == 1 :
             try :
@@ -42,7 +47,25 @@ if __name__ == "__main__":
 
         elif op == 3:
             try:
-                cpf = int(input(" insira o cpf que deseja encontrar"))
-                print(buscar(pessoas,cpf))
+                cpf = input(" insira o cpf que deseja encontrar")
+                print(buscar_cpf(pessoas,cpf))
             except:
                 print("dado invalido ")
+        
+
+        elif op == 4:
+            try:
+                num = int(input(" insira o num que deseja encontrar"))
+                print(buscar_tele(pessoas,num))
+            except:
+                print("dado invalido ")
+        
+        elif op == 5:
+            try:
+                item = input('coloque o cpf que deseja apagar')
+                pessoas.pop(item)
+                print(pessoas)
+
+            except:
+                print('dado invalido')    
+
