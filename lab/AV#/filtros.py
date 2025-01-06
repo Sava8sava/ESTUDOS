e4a2dc96e6763234e38280a81defc24180b30bda
sava8sava <supremomelao@gmail.com>
2025-01-06 10:02:01 -0300

    modificaçoes em lab

---
+++ lab/AV#/filtros.py
@@ -0,0 +1,15 @@
+from PIL import Image
+
+#classe filtros:
+
+#filtro preto e branco 
+
+#filtro cartoon
+
+#escala de cinza
+
+#filtro negativo 
+
+#modo contorno
+
+#modo blurred
--- lab/AV#/teste.py
+++ lab/AV#/teste.py
@@ -3,5 +3,6 @@ from Imagem import Imagem
 imagem = Imagem()
 imagem.set_public_img(r"https://www.revistaveterinaria.com.br/wp-content/uploads/2018/11/gatinho-preguicoso1.jpg")
 print(imagem.get_img_path())
-imagem.set_local_img(r"imagens teste/Captura de imagem_20250105_103714.png")
-print(imagem.get_img_path())
+imagem.show_image()
+print(imagem.get_img_extension())
+
